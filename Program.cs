cec5c36b31ecad3afe127bba9fe6917751d8966a
IronJoo <joanaferro92@gmail.com>
2021-03-01 13:08:45 +0000

    Exercise 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace w3ExArrays //{ }
+{
+    class Exercise1
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Insert a number: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You inserted the following numbers");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
